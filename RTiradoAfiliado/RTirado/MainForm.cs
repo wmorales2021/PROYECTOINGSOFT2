4d06051a018d8478fc60f58cc00374de3a132053
wilson morales <wmorales2500@gmail.com>
2021-03-07 20:52:38 -0500

    agregamos archivos proyecto RTiradoAfiliado

---
+++ RTiradoAfiliado/RTirado/MainForm.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lenovo
+ * Date: 26/09/2019
+ * Time: 4:59 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace RTirado
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string extension = ".PDF";
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void ListAllFiles()
+		{
+			foreach (string path in Directory.EnumerateFiles(Directory.GetCurrentDirectory()))
+	        {
+				lstArchivosOrigen.Items.Add(path);
+	        }
+			
+		}
+		void CreateDirectoryAndCopyFile()
+		{
+			Directory.CreateDirectory(txtCarpetaDestino.Text);
+			
+			foreach (string fileName in lstArchivosOrigen.Items) {
+				string result = Path.GetFileNameWithoutExtension(fileName);
+				string result2 = Path.GetFileName(fileName);
+				
+				string newFolder = string.Format("{0}\\{1}",txtCarpetaDestino.Text , result);
+				string e = Path.GetExtension(fileName);
+				Directory.CreateDirectory(newFolder);
+				lstDestino.Items.Add(newFolder);
+				File.Copy(fileName,string.Format("{0}\\{1}", newFolder, result2), true);
+			}
+		}
+		void BtnCarpetaOrigenClick(object sender, EventArgs e)
+		{
+			if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				txtCarpetaOrigen.Text = folderBrowserDialog1.SelectedPath;
+				txtCarpetaDestino.Text = folderBrowserDialog1.SelectedPath + "\\RESULTADO";
+				Directory.SetCurrentDirectory(folderBrowserDialog1.SelectedPath);
+				//Debug.Print(Directory.GetCurrentDirectory());
+				ListAllFiles();
+				
+			}
+		}
+		void BtnCarpetaDestinoClick(object sender, EventArgs e)
+		{
+			if(folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+			{
+				txtCarpetaDestino.Text = folderBrowserDialog1.SelectedPath ;
+			}
+		}
+		void ProcesarToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			if(MessageBox.Show("Desea procesar los archivos", "PROCESAR ARCHIVOS", MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+			{
+				CreateDirectoryAndCopyFile();
+			}
+		}
+	}
+}
---
+++ RTiradoAfiliado/RTirado/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lenovo
+ * Date: 26/09/2019
+ * Time: 4:59 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace RTirado
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
